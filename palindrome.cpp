d9ad3e5a44b21ca900f7fc9f052213f341c64ff5
alekseypavlov4 <97864682+alekseypavlov4@users.noreply.github.com>
2022-03-24 20:50:17 +1000

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int qwe(int a);
+
+int pln(int a);
+
+int main()
+{
+	int a;
+	cout<<"Input: ";
+	cin>>a;
+	
+	qwe(a);
+	pln(a);
+	
+	return 0;
+}
+
+int qwe(int a)
+{
+	int t = a;
+    int b = 0;
+    
+    while (tp != 0)
+    {
+        b = b * 10 + temp % 10;
+        t /= 10;
+    }
+    
+	cout<<b<<endl;
+	
+	return 0;
+}
+
+int pln(int a)
+{	    
+    int t = a;
+    int b = 0;
+    
+    while (t != 0)
+    {
+        b = b * 10 + t % 10;
+        t /= 10;
+    }
+    
+    if (a == b)
+	{
+        cout<<"palindrome"<<endl;
+    }
+    else
+	{
+        cout<<"not a palindrome"<<endl;
+    }
+    
+    return 0;
+}
