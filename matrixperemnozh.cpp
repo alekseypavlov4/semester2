73043040c401e3258ce5bd62d44f7bd9e1d4177f
alekseypavlov4 <97864682+alekseypavlov4@users.noreply.github.com>
2022-05-05 12:34:44 +1000

    Update matrixperemnozh.cpp

--- matrixperemnozh.cpp
+++ matrixperemnozh.cpp
@@ -9,6 +9,7 @@ int main()
     int i = 1;
     while (i != 0)
     {
+    cout << endl;
     cout << "stolbci matrix A: ";
     cin >> a;
     cout << "srtoki matrix A: ";
@@ -19,59 +20,67 @@ int main()
     cout << "stroki matrix B: ";
     cin >> d;
 
-    if ((a == d) or (b == c) or (a == b == c == d))
-    {
-        i = 0;
-    }
-    }
+    cout << endl;
 
+    if ((a == d) or (b == c) or (a == b == c == d))
+        {
+        cout << "Uslovie vipolneno" << endl;
+        i = 0;
+        }
+    else
+        {
+            cout << "Uslovie ne vipolneno, povtorite snova" << endl;
+        }
+    }
+    
+    cout << "Vvedite elenti matrix A" << endl;
     int A[a][b];
     for(int i = 0; i < b; i++)
-    {
-        for(int j = 0; j < a; j++)
         {
+        for(int j = 0; j < a; j++)
+            {
             cin >> A[i][j];
+            }
         }
-    }
 
     cout << endl;
-
+    cout << "Vvedite elenti matrix B" << endl;
     int B[c][d];
     for(int i = 0; i < d; i++)
-    {
-        for(int j = 0; j < c; j++)
         {
+        for(int j = 0; j < c; j++)
+            {
             cin >> B[i][j];
+            }
         }
-    }
 
     cout << endl;
 
     for(int i = 0; i < b; i++)
-    {
-        for(int j = 0; j < a; j++)
         {
+        for(int j = 0; j < a; j++)
+            {
             cout << setw(3) << A[i][j] << " ";
             if (j >= (a -1))
-            {
+                {
                 cout << endl;
+                }
             }
         }
-    }
 
     cout << endl;
 
     for(int i = 0; i < d; i++)
-    {
-        for(int j = 0; j < c; j++)
         {
+        for(int j = 0; j < c; j++)
+            {
             cout << setw(3) << B[i][j] << " ";
             if (j >= (c -1))
-            {
+                {
                 cout << endl;
+                }
             }
         }
-    }
 
     return 0;
 }
