93cbd80f0a2dc2d67492c7ea44f8c707814965d8
alekseypavlov4 <97864682+alekseypavlov4@users.noreply.github.com>
2022-05-05 12:27:53 +1000

    Create matrixperemnozh.cpp

---
+++ matrixperemnozh.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    int i = 1;
+    while (i != 0)
+    {
+    cout << "stolbci matrix A: ";
+    cin >> a;
+    cout << "srtoki matrix A: ";
+    cin >> b;
+
+    cout << "stolbci matrix B: ";
+    cin >> c;
+    cout << "stroki matrix B: ";
+    cin >> d;
+
+    if ((a == d) or (b == c) or (a == b == c == d))
+    {
+        i = 0;
+    }
+    }
+
+    int A[a][b];
+    for(int i = 0; i < b; i++)
+    {
+        for(int j = 0; j < a; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << endl;
+
+    int B[c][d];
+    for(int i = 0; i < d; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            cin >> B[i][j];
+        }
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < b; i++)
+    {
+        for(int j = 0; j < a; j++)
+        {
+            cout << setw(3) << A[i][j] << " ";
+            if (j >= (a -1))
+            {
+                cout << endl;
+            }
+        }
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < d; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            cout << setw(3) << B[i][j] << " ";
+            if (j >= (c -1))
+            {
+                cout << endl;
+            }
+        }
+    }
+
+    return 0;
+}
